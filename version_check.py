7c6dfbb8948041638874ed41bfcc79c4dade71fd
dschloe <widm@naver.com>
2022-11-07 12:43:45 +0900

    updated

---
+++ version_check.py
@@ -0,0 +1,11 @@
+import pyspark
+import numpy as np
+import pandas as pd
+import matplotlib as mlp
+import seaborn as sns
+
+print(pyspark.__version__)
+print(np.__version__)
+print(pd.__version__)
+print(mlp.__version__)
+print(sns.__version__)
